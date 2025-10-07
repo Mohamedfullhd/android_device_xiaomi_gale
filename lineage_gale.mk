595649a9fe097487de9d4f56e3e2bdb5c7088b5b
Mohamedfullhd <aaamohamed312@gmail.com>
2025-10-07 18:17:14 +0300

    Update lineage_gale.mk

--- lineage_gale.mk
+++ lineage_gale.mk
@@ -25,3 +25,20 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="vnd_gale-user 12 SP1A.210812.016 V816.0.5.0.UGPMIXM release-keys"
     BuildFingerprint=alps/vnd_gale/gale:12/SP1A.210812.016/V816.0.5.0.UGPMIXM:user/release-keys
+
+# To include Gapps 
+WITH_GMS := true
+
+# To Build Google(Dailer, Message, Phone) and BCR
+WITH_GMS_COMMS_SUITE := true
+
+# To Add cinematic wallpaer support (only supported in gapps build not in vanilla remove this flag for vanilla builds)
+TARGET_SUPPORTS_WALLEFFECT := true
+
+#Some more GMS Flag
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_INCLUDE_STOCK_AICORE := true
+
+# To Add Bypass Charging Support (need to be adapted in DT and KT as well)
+BYPASS_CHARGE_SUPPORTED  := false
